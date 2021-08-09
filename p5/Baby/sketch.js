6d91fc1588ca416213e82bb9d30ab7d7cd0318bb
Olaoluwa07 <54251701+Olaoluwa07@users.noreply.github.com>
2021-08-09 18:41:00 -0500

    gf

--- p5/Baby/sketch.js
+++ p5/Baby/sketch.js
@@ -1,7 +1,53 @@
+// Run this example on a mobile device
+// Rotate the device by 90 degrees in the
+// X-axis to change the value.
+
+var value = 0;
+var mic, recorder, soundFile;
+
+var state = 0; // mousePress will increment from Record, to Stop, to Play
+
 function setup() {
-  // put setup code here
+  createCanvas(400,400);
+  background(255,0,0);
+  fill(0);
+
+  // create an audio in
+  mic = new p5.AudioIn();
+
+  // users must manually enable their browser microphone for recording to work properly!
+  mic.start();
+
+  // create a sound recorder
+  recorder = new p5.SoundRecorder();
+
+  // connect the mic to the recorder
+  recorder.setInput(mic);
+
+  // create an empty sound file that we will use to playback the recording
+  soundFile = new p5.SoundFile();
 }
 
 function draw() {
-  // put drawing code here
+    text('Enable mic and click the mouse to begin recording', 100, 100);
+  fill(value);
+  rect(25, 25, 50, 50);
+  print(value);
+}
+
+function deviceTurned() {
+  if (turnAxis === 'X') {
+    if (value === 0) {
+      recorder.record(soundFile);
+      value = 255;
+    }
+    else if (value === 255) {
+      recorder.stop();
+    //  soundFile.play();
+      value = 0;
+    }
+  }
+  else if (turnAxis === 'Y') {
+    soundFile.play();
+  }
 }
